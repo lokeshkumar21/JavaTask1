678d17835df6d46dc12c33669cc746fe016e6ac5
Lokesh Kumar K <lokeshkumarlk2105@gmail.com>
2026-08-23 10:33:43 +0530

    Updated

---
+++ Task1/src/NonPrimitive.java
@@ -0,0 +1,11 @@
+public class NonPrimitive {
+
+    public static void main(String[] args) {
+        String name = "Lokesh Kumar K";
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        System.out.println(name);
+        System.out.println(numbers[4]);
+
+    }
+}
---
+++ Task1/src/PrimitiveDataType.java
@@ -0,0 +1,24 @@
+public class PrimitiveDataType {
+
+    public static void main(String[] args) {
+
+        byte reg_no = 25;
+        short dep_code = 104;
+        int age =21;
+        long fees = 55000;
+        float cgpa = 8.25f;
+        double average = 7.43455345;
+        char grade = 'O';
+        boolean isPass = true;
+
+        System.out.println(reg_no);
+        System.out.println(dep_code);
+        System.out.println(age);
+        System.out.println(fees);
+        System.out.println(cgpa);
+        System.out.println(average);
+        System.out.println(grade);
+        System.out.println(isPass);
+
+    }
+}
